152ec333ecaf5958961a8d14678575d15b7aa369
Rasmus Persson <rrasmuspersson@gmail.com>
2021-02-23 09:45:47 +0100

    Added more API keys and IDs and added styling to the css cards.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const main = document.getElementById('main');
 
-const applicationID = "4fcdd240";
-// const applicationKey1= "b85adca58547211f75cf9c3a47cc424d";
+//const applicationID = "4fcdd240";
+// const applicationKey = "86fbda45bf039fe0a2dd29fdedc2f8d8";
+
+const applicationKey = "b85adca58547211f75cf9c3a47cc424d";
+const applicationID = "7bfe09bb";
+
+// const applicationKey= "6c641ba9d7108ec30c554da9cc973757";
+// const applicationID = "1b5c3465";
 
 
-const applicationKey = "86fbda45bf039fe0a2dd29fdedc2f8d8";
 // const applicationKey= "a82c3e69b3ddce206012513a57e2f8d9";
 
 // Skapa en funktion likt generateBoard() från Guess Who som syns från början.
@@ -41,14 +46,40 @@ fetch(queryString)
       const ingredients = hits.recipe.ingredientLines;
 
 
+      {
+        /* <h1>${label}</h1><br>
+              <img src=${image}>
+              <p>Cooking time:${fixedCookingTime(cookingTime)}</p>
+              <p>Source:${source}</p>
+              <p>Link to recipe: ${linkToRecipe}</p>
+              <p>Ingredients: ${ingredients}</p> */
+      }
+
 
       main.innerHTML += `
-      <h1>${label}</h1><br>
+      
+      <div id="cssCard" class="css-card">
+      <div id="cssCardImg" class="css-card-image">
       <img src=${image}>
-      <p>Cooking time:${fixedCookingTime(cookingTime)}</p>
-      <p>Source:${source}</p>
-      <p>Link to recipe: ${linkToRecipe}</p>
-      <p>Ingredients: ${ingredients}</p>
+      </div>
+      <div id="cssCardContent" class="css-card-content">
+        <div id="cssCardContentTitle" class="css-card-content-title">
+        <h2>${label}</h2><br>
+        <p>Cooking time: ${fixedCookingTime(cookingTime)}</p>
+        </div>
+        <div id="cssCardContentType" class="css-card-content-type">
+          <p>Type: Beef/Chicken/Vego</p>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
       `
 
     })
