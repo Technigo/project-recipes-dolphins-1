fc38eb6a0b13c6a659bc14761a34ed850087a3e6
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-02-23 10:28:32 +0100

    text

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const main = document.getElementById('main');
 const searchButton = document.getElementById('searchButton')
+const cssCardGrid = document.getElementById("cssCardGrid");
 
-const applicationID = "4fcdd240";
-// const applicationKey1= "b85adca58547211f75cf9c3a47cc424d";
+//const applicationID = "4fcdd240";
+// const applicationKey = "86fbda45bf039fe0a2dd29fdedc2f8d8";
 
+const applicationKey = "b85adca58547211f75cf9c3a47cc424d";
+const applicationID = "7bfe09bb";
 
-const applicationKey = "86fbda45bf039fe0a2dd29fdedc2f8d8";
-// const applicationKey= "a82c3e69b3ddce206012513a57e2f8d9";
+// const applicationKey= "6c641ba9d7108ec30c554da9cc973757";
+// const applicationID = "1b5c3465";
+
 
 // Skapa en funktion likt generateBoard() från Guess Who som syns från början.
 // När man sedan söker/filtrerar så genereras en annan innerHTML med sökresultatet
@@ -31,7 +35,6 @@ const fixedCookingTime = ((cookingTime) => {
 fetch(queryString)
   .then((response) => {
     return response.json()
-    console.log(response)
   })
   .then((json) => {
     console.log(json)
@@ -44,14 +47,40 @@ fetch(queryString)
       const ingredients = hits.recipe.ingredientLines;
 
 
+      {
+        /* <h1>${label}</h1><br>
+              <img src=${image}>
+              <p>Cooking time:${fixedCookingTime(cookingTime)}</p>
+              <p>Source:${source}</p>
+              <p>Link to recipe: ${linkToRecipe}</p>
+              <p>Ingredients: ${ingredients}</p> */
+      }
 
-      main.innerHTML += `
-      <h1>${label}</h1>
+
+      cssCardGrid.innerHTML += `
+
+      <div id="cssCard" class="css-card">
+      <div id="cssCardImg" class="css-card-image">
       <img src=${image}>
-      <p>Cooking time:${fixedCookingTime(cookingTime)}</p>
-      <p>Source:${source}</p>
-      <p>Link to recipe: ${linkToRecipe}</p>
-      <p>Ingredients: ${ingredients}</p>
+      </div>
+      <div id="cssCardContent" class="css-card-content">
+        <div id="cssCardContentTitle" class="css-card-content-title">
+        <h2>${label}</h2>
+        <p>Cooking time: ${fixedCookingTime(cookingTime)}</p>
+        </div>
+        <div id="cssCardContentType" class="css-card-content-type">
+          <p>Type: Beef/Chicken/Vego</p>
+        </div>
+      </div>
+      </div>
+
+
+
+
+
+
+
+
       `
 
     })
